b400a380d3d1901d704549618cc042b096f11bf5
mostafijur-attainu <89730481+mostafijur-attainu@users.noreply.github.com>
2021-10-28 14:20:40 +0530

    Update two pointers.py

--- two pointers.py
+++ two pointers.py
@@ -1,26 +1,19 @@
-# Frequency of each element of an array
+# Two pointers -- Longest subarray with all integers same
 
-arr = [5, 9, 5, 4, 9, 6, 6] # 5 : 2 | 9 : 2 | 4 : 1 | 6 : 2
-# Brute force
-# for i in range(len(arr)):
-#     count = 1
+# arr = [1, 2, 2, 3, 2]
 
-#     if arr[i] != -1 :
-#         for j in range(i+1, len(arr)):
-#             if arr[i] == arr[j]:
-#                 count += 1
-#                 arr[j] = -1
+# i = 0
+# ans = 1
+
+# while i < len(arr):
+
+#     j = i+1
+
+#     while (j < len(arr)) and (arr[j] == arr[j-1]):
+#         j += 1
     
-#         print(arr[i], count)
-
-#Hashing
-d = {}
-
-for i in range(len(arr)):
-
-    if arr[i] in d.keys():
-        d[arr[i]] += 1
-    else:
-        d[arr[i]] = 1
-
-print(d)
+#     if ans < j-i:
+#         ans = j-i 
+    
+#     i = j
+# print(ans)
